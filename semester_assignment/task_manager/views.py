f1ca4c97bbb85ea96f14c901ea5c0ba5e4ad28b6
Eruerufusenpai <martin.faeroy@gmail.com>
2020-11-03 16:53:01 +0100

    Fixed main-page template so that it actually works.

--- semester_assignment/task_manager/views.py
+++ semester_assignment/task_manager/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
 
+tasks = [
+    {
+        "post_id": 0,
+        "general_description": "Move these boxes!",
+        "detailed Description": None,
+        "is_important": False,
+        "is_completed": False, 
+        "time_stamp": "348535932762",
+        "author": "5",
+        "expertise": None
+    },
+    {
+        "post_id": 1,
+        "general_description": "Move these god damn boxes!",
+        "detailed Description": None,
+        "is_important": True,
+        "is_completed": False, 
+        "time_stamp": "348535933005",
+        "author": "5",
+        "expertise": None
+    },
+]
+
 def create_post(request):
     return render(request, 'task_manager/create_post.html')
 
 def main_page(request):
-    return render(request, 'task_manager/main_page.html')
+    context = {
+        "Tasks": tasks
+    }
+
+    return render(request, 'task_manager/main_page.html', context)
 
 def settings(request):
     return render(request, 'task_manager/settings.html')
