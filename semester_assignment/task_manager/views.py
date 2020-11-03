1070d4e57f30e27e89bd33d2335d18bddd0f95e7
Eruerufusenpai <martin.faeroy@gmail.com>
2020-11-03 14:04:18 +0100

    bugfixes

--- semester_assignment/task_manager/views.py
+++ semester_assignment/task_manager/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def create_post(request):
-    return render(request, 'tast_manager/create_post.html')
+    return render(request, 'task_manager/create_post.html')
 
 def main_page(request):
     return render(request, 'task_manager/main_page.html')
